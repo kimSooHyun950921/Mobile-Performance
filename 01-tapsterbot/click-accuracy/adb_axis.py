2bbf446b1b224f15156b640c9207abe4ce6fec2e
kimSooHyun950921 <soohyunkim950921@gmail.com>
2020-06-28 22:13:46 +0900

    add module for change adb coordiinates hex to deci

---
+++ 01-tapsterbot/click-accuracy/adb_axis.py
@@ -0,0 +1,24 @@
+import re
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import sys
+def main():
+    command = "adb shell getevent -lt /dev/input/event0 | grep 'ABS_MT_POSITION'"
+    regex = r'ABS_MT_POSITION_(\w)\s+(\d+\w+)'
+    process = Popen(command, shell=True,
+                    stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+
+    while True:
+        result = process.stdout.readline().decode().replace('\n','')
+        matches = re.findall(regex, result)
+        for axis, value in matches:
+            deci_value = int(value, 16)
+            if axis == 'X':
+                displayX = deci_value * 1440 / 4096
+                print(axis, displayX)
+            if axis == 'Y':
+                displayY = deci_value * 2960 / 4096
+                print(axis, displayY) 
+
+if __name__ == "__main__":
+    main()
