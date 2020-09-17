b6181d92f13bd27fbf1a7c2acdab49436448c40b
kimSooHyun950921 <soohyunkim950921@gmail.com>
2020-09-17 09:26:31 +0900

    add recording-streaming

---
+++ 02-image_detection/06-servingtest/sub_pub_test/streaming_detection.py
@@ -0,0 +1,112 @@
+import camera
+from flask import Flask, render_template, Response, request, redirect, url_for, jsonify
+import camera
+import cv2
+import subprocess
+from execjs import get
+import os
+import time
+import zmq
+import requests
+#import predict_clickable as pc
+video_camera = None
+recording_camera = None
+app = Flask(__name__) 
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+
+def generate_axis(width, height):
+    start = (i, j)
+    end = ()
+
+
+def draw_subline(width, height, vis):
+    line_list = generate_axis(width, height)
+    for i in line_list:
+        cv2.line(vis, i[0], i[1], (154, 231, 197) ,1)
+    return vis
+
+
+def gn():
+    global video_camera
+    video_camera = camera.VideoStreaming()
+    mser = cv2.MSER_create()
+    out = ''
+    num = 0
+
+    while True:
+        frame = video_camera.get_frame()
+        ret, jpg = cv2.imencode('.JPEG', frame)
+        jpg_bytes = jpg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n\r\n')
+    cam.cap_res.release()
+    video.release()
+
+@app.route('/video_feed')
+def video_feed():
+    print(type(gn()))
+    return Response(gn(),
+                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def get_path(file_name):
+    device = file_name.split('_')[0]
+    mp4_name = file_name.split('_')[1]
+    default_path = '/home/kimsoohyun/00-Research/02-Graph/05-data/mp4'  
+    path = os.path.join(default_path, device,'{}.avi'.format(file_name))
+    return path
+
+def change_name(mp4_name):
+    try:
+        file_path = '/home/kimsoohyun/00-Research/02-Graph/data/cut_point'
+        file_name = 'cut_info.csv'
+        origin_file_name = os.path.join(file_path, file_name)
+        new_file_name = os.path.join(file_path, mp4_name + '.csv')
+        print(new_file_name)
+        os.rename(origin_file_name, new_file_name)
+    except Exception as e:
+        pass
+
+
+def send_starttime():
+    #===========CHANGE=========#
+    print("SEND STARTTIME")
+    ip = 'http://localhost:8888'
+    starttime = int(time.time())
+    data = {"starttime": starttime}
+    res = requests.post(ip, data=data)
+    print(res)
+
+
+@app.route('/record_status', methods=['POST'])
+def start_record():
+    global recording_camera
+    json = request.get_json()
+    status = json['status']
+    filename = json['filename']
+    print("status:", status," filename: ",  filename)
+    fullpath = get_path(filename)
+
+    if recording_camera == None:
+        recording_camera = camera.Recording(fullpath)
+
+    print("FULL PATH",fullpath)
+    if status == "true":
+        send_starttime()
+        print("DEBUG video_camera")
+        recording_camera.write()
+        print("rescord start")
+        return jsonify(result="started")
+    elif status == "false":
+        recording_camera.__del__()
+        #del recording_camera
+        recording_camera = None
+        return jsonify(result="stopped")
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', debug=True,port=8090, threaded=True)
