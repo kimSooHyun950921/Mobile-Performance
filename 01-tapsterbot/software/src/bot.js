b94e00c478d5057208a418407683be5bfba5a069
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-04-21 23:46:03 +0900

    add moving tapterbot

---
+++ 01-tapsterbot/software/src/bot.js
@@ -0,0 +1,395 @@
+five = require("johnny-five");
+kinematics = require("./kinematics");
+svgRead = require("./SVGReader");
+drawing = require("./draw");
+arc = require("./arc");
+motion = require("./motion");
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var jsonParser = bodyParser.json();
+
+app.get('/', function (req, res) {
+  res.send('Welcome to Tapster World!');
+});
+
+var servo1;
+var servo2;
+var servo3;
+
+app.post('/go', jsonParser, function (req, res) {
+  console.log('Go!');
+  //console.log(req.body);
+  if ((typeof req.body.x === 'undefined') ||
+     (typeof req.body.y === 'undefined') ||
+     (typeof req.body.z === 'undefined')) {
+
+     res.status(400).send('Invalid request\n');
+
+  } else {
+    var x = req.body.x;
+    var y = req.body.y;
+    var z = req.body.z;
+    // Move with default easing
+    go(x, y, z);
+
+    // Move with no easing
+    //go(x, y, z, 'none');
+
+    setTimeout(function(){ res.send('OK\n') }, 1250);
+  }
+
+});
+
+app.post('/circle', jsonParser, function (req, res) {
+  console.log('Circle!');
+  console.log(req.body);
+  if ((typeof req.body.x === 'undefined') ||
+     (typeof req.body.y === 'undefined') ||
+     (typeof req.body.z === 'undefined') ||
+     (typeof req.body.radius === 'undefined') ||
+     (typeof req.body.startAngle === 'undefined') ||
+     (typeof req.body.anticlockwise === 'undefined') ||
+     (typeof req.body.delay === 'undefined') ||
+     (typeof req.body.rotations === 'undefined')) {
+
+     res.status(400).send('Invalid request\n');
+
+  } else {
+    var centerX = req.body.x;
+    var centerY = req.body.y;
+    var centerZ = req.body.z;
+    var radius = req.body.radius;
+    var startAngle = req.body.startAngle;
+    var anticlockwise = req.body.anticlockwise;
+    var delay = req.body.delay;
+    var rotations = req.body.rotations;
+
+    // An array to save points on the arc
+    var points = arc(centerX, centerY, centerZ, radius, startAngle, startAngle, anticlockwise);
+
+    // Go to each point in the arc
+    for (var rotation = 0; rotation < rotations; rotation += 1) {
+      for (var i = 0; i < points.length; i += 1) {
+        setTimeout( function(point) {
+          go(point.x, point.y, point.z, "none")
+        },
+        i*delay + points.length*delay*rotation,
+        points[i]);
+      }
+    }
+
+    // Return
+    setTimeout(function(){ res.send('OK\n') }, points.length*delay*rotations );
+  }
+});
+
+
+app.post('/arc', jsonParser, function (req, res) {
+  console.log('Arc!');
+  console.log(req.body);
+  if ((typeof req.body.x === 'undefined') ||
+     (typeof req.body.y === 'undefined') ||
+     (typeof req.body.z === 'undefined') ||
+     (typeof req.body.radius === 'undefined') ||
+     (typeof req.body.startAngle === 'undefined') ||
+     (typeof req.body.endAngle === 'undefined') ||
+     (typeof req.body.anticlockwise === 'undefined') ||
+     (typeof req.body.delay === 'undefined')) {
+
+     res.status(400).send('Invalid request\n');
+
+  } else {
+    var centerX = req.body.x;
+    var centerY = req.body.y;
+    var centerZ = req.body.z;
+    var radius = req.body.radius;
+    var startAngle = req.body.startAngle;
+    var endAngle = req.body.endAngle;
+    var anticlockwise = req.body.anticlockwise;
+    var delay = req.body.delay;
+
+    // An array to save points on the arc
+    var points = arc(centerX, centerY, centerZ, radius, startAngle, endAngle, anticlockwise);
+
+    // Go to each point in the arc
+    var i;
+    for (i = 0; i < points.length; i += 1) {
+      setTimeout( function(point) { go(point.x, point.y, point.z, "none") }, i*delay, points[i]);
+    }
+
+    // Return
+    setTimeout(function(){ res.send('OK\n') }, i*delay );
+  }
+
+});
+
+
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+  console.log('Tapster web server listening on port ' + port);
+});
+
+
+//If a filepath is specified, load that config
+//Otherwise, resort to the default config
+//> Usage:
+//> node bot.js "C:\Projects\Tapsterbot\software\config.js"
+if (process.argv[2]) {
+  try {
+    var config = require(process.argv[2]);
+    console.log("Config found and loaded.");
+  } catch (e) {
+    console.log("Config not found. Loading default.");
+    var config = require("../config.js");
+  }
+}
+else {
+  console.log("Config not specified. Loading default.");
+  var config = require("../config.js");
+}
+
+//Alternate config loading code
+//If a Tapster version is specified, load that config
+//Otherwise resort to the default config
+//> Usage:
+//> node bot.js "Tapster-2-plus"
+
+/*if (process.argv[2]) {
+  try {
+    var config = require("../" + process.argv[2] + ".js");
+    console.log("Config found and loaded.");
+  } catch (e) {
+    console.log("Config not found. Loading default.");
+    var config = require("../config.js");
+  }
+}
+else {
+  console.log("Config not specified. Loading default.");
+  var config = require("../config.js");
+} */
+
+k = new kinematics.Kinematics({
+  e: config.e,
+  f: config.f,
+  re: config.re,
+  rf: config.rf
+});
+
+svg = new svgRead.SVGReader({
+  baseWidth: config.baseWidth,
+  baseHeight: config.baseHeight,
+  drawHeight: config.drawHeight,
+  delay: config.delay,
+  defaultEaseType: config.defaultEaseType
+});
+
+draw = new drawing.Draw({
+  baseWidth: config.baseWidth,
+  baseHeight: config.baseHeight,
+  drawHeight: config.drawHeight,
+  defaultEaseType: config.defaultEaseType
+});
+
+board = new five.Board({
+  debug: true,
+  port: "/dev/ttyACM0"
+});
+
+var steps = 5;
+var delay = config.delay / steps;
+var defaultEaseType = config.defaultEaseType;
+
+current = [0, 0, -140];
+timer = 0;
+
+board.on("ready", function() {
+    // Setup
+    /*servo1 = five.Servo({
+        address: 0x40,
+        controller: "PCA9685",
+        pin: 0,
+        range: [35, 145] //Too high of a minimum input will cause issues with the forward kinematics
+    });
+    servo2 = five.Servo({
+        address: 0x40,
+        controller: "PCA9685",
+        pin: 1,
+        range: [35, 145]
+    });
+    servo3 = five.Servo({
+        address: 0x40,
+        controller: "PCA9685",
+        pin: 2,
+        range: [35, 145]
+    }); */
+
+  servo1 = five.Servo({
+      pin: 9,
+      range: [0, 100]
+  });
+
+  servo2 = five.Servo({
+      pin: 10,
+      range: [0, 100]
+  });
+
+  servo3 = five.Servo({
+      pin: 11,
+      range: [0, 100]
+  });
+
+    board.repl.inject({
+      servo1: servo1,
+      s1: servo1,
+      servo2: servo2,
+      s2: servo2,
+      servo3: servo3,
+      s3: servo3,
+    });
+
+    // Move to starting point
+    var max = 15;
+    var min = 5;
+    var range = max - min;
+    servo1.to(15);
+    servo2.to(15);
+    servo3.to(15);
+
+    
+    var dance = function() {
+      servo1.to(parseInt((Math.random() * range) + min, 10));
+      servo2.to(parseInt((Math.random() * range) + min, 10));
+      servo3.to(parseInt((Math.random() * range) + min, 10));
+    };
+
+    var dancer;
+
+    start_dance = function() {
+      if (!dancer) dancer = setInterval(dance, 250);
+    }
+
+    stop_dance = function() {
+      if (dancer) {
+        clearInterval(dancer);
+        dancer = null;
+      }
+    }
+
+    board.repl.inject({
+      dance: start_dance,
+      chill: stop_dance
+    }); 
+
+
+});
+
+Number.prototype.map = function ( in_min , in_max , out_min , out_max ) {
+  return ( this - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+}
+
+rotate = function(x,y) {
+    var theta = -60;
+    x1 = x * cos(theta) - y * sin(theta);
+    y1 = y * cos(theta) + x * sin(theta);
+    return [x1,y1]
+}
+
+reflect = function(x,y) {
+    var theta = 0;
+    x1 = x;
+    y1 = x * sin(2*theta) - y * cos(2*theta);
+    return [x1,y1]
+}
+
+
+// A sine function for working with degrees, not radians
+sin = function(degree) {
+    return Math.sin(Math.PI * (degree/180));
+}
+
+// A cosine function for working with degrees, not radians
+cos = function(degree) {
+  return Math.cos(Math.PI * (degree/180));
+}
+
+moveServosTo = function(x, y, z) {
+  current = [x, y, z]
+  //console.log("current: "+ current);
+  reflected = reflect(x,y);
+  //console.log("reflected: "+reflected);
+  rotated = rotate(reflected[0],reflected[1]);
+  //console.log("roated: "+rotated);
+  angles = k.inverse(rotated[0], rotated[1], z);
+  //console.log("angles: "+angles);
+  //console.log("");
+  servo1.to((angles[1]).map(config.servo1.in_min, config.servo1.in_max, config.servo1.out_min, config.servo1.out_max));
+  servo2.to((angles[2]).map(config.servo2.in_min, config.servo2.in_max, config.servo2.out_min, config.servo2.out_max));
+  servo3.to((angles[3]).map(config.servo3.in_min, config.servo3.in_max, config.servo3.out_min, config.servo3.out_max));
+  //console.log("SERVO angles:"+angles);
+  //console.log((angles[1]).map(config.servo1.in_min, config.servo1.in_max, config.servo1.out_min, config.servo1.out_max));
+  //console.log((angles[2]).map(config.servo2.in_min, config.servo2.in_max, config.servo2.out_min, config.servo2.out_max));
+  //console.log((angles[3]).map(config.servo3.in_min, config.servo3.in_max, config.servo3.out_min, config.servo3.out_max));
+}
+
+
+function pause(time){
+    var i;
+    console.log("PAUSE: "+ time)
+    for(i = 0; i < time; i++){
+        //console.log(i)
+    }
+}
+
+
+var go = function(x, y, z, easeType) {
+  console.log('Greeting from bot.js!'+x+", "+y+", "+z);
+  var pointB = [x, y, z];
+  if (easeType == "none") {
+    moveServosTo(pointB[0], pointB[1], pointB[2]);
+    return; //Ensures that it doesn't move twice
+  }
+  else if (!easeType){
+    easeType = defaultEaseType //If no easeType is specified, go with default (specified in config.js)
+  }
+  //motion.move(current, pointB, steps, easeType, delay);
+  var points = motion.getPoints(current, pointB, steps, easeType);
+  for (var i = 0; i < points.length; i++) {
+    setTimeout( function(point) { 
+      moveServosTo(point[0], point[1], point[2]) }, 
+      i * delay, points[i]);
+  }
+}
+
+//Returns the coordinates of the end effector, based on the angles
+//Using the map function messes up these values
+//Simply passing in the original angles will return the correct coordinates
+position = function() {
+  return k.forward(servo1.last.degrees, servo2.last.degrees, servo3.last.degrees);
+}
+
+//A separate setTimeout method so that delays work properly
+doSetTimeout = function(x, y, z, timeDelay, easing) {
+  if (!easing)
+    easing = defaultEaseType;
+
+  setTimeout(function() { go(x, y, z, easing) }, timer);
+  timer = timer + timeDelay;
+};
+
+
+resetTimer = function() {
+  timer = 0;
+}
+
+module.exports = {
+    go,
+    pause,
+    moveServosTo
+}
--- 01-tapsterbot/software/webserver/scenario.js
+++ 01-tapsterbot/software/webserver/scenario.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var bot = require('../src/bot.js');
 var waitSync = require('wait-sync');
 var csv_writer = require('csv-writer');
-var bot_move = require('./click-event.js');
+var bot_move = require('./click-event-timeout.js');
+var bot_move_time = 0
 const { parse } = require('querystring');
 const { cors } = require('cors');
 
@@ -13,7 +14,9 @@ console.log('another server opened');
 
 
 var csv_data = [];
-
+let rcindex = 0;
+let start_time = 0;
+let is_going = false
 var data = 'data';
 var contentMap = {
     '/': 'scene.html',
@@ -21,15 +24,16 @@ var contentMap = {
     '/recorder.js': 'recorder.js',
     '/analysis': 'analysis.html'
     }
-var options = {                                                                 
+
+var options = {
     host: "localhost",
     port: 8090,
-    path: "/recording",                                                                   
-    method: "GET",                                                             
-    headers: {                                                                  
+    path: "/recording",
+    method: "GET", 
+    headers: {
         "Content-Type": "applciation/x-www-form-urlencoded",
         'Content-Length': Buffer.byteLength(data)
-        }                                                                       
+        }
  };   
 
 var predict_options = {
@@ -42,6 +46,7 @@ var predict_options = {
 
 predict_callback = function(response){
   var str = ''
+  console.log("REQUEST START")
   response.on('data', function(chunk){
     str += chunk
   });
@@ -49,31 +54,49 @@ predict_callback = function(response){
     console.log(str);
     var obj = JSON.parse(str);
     var box_list = obj.box;
-    console.log(Array.isArray(box_list))
-    for(i = 0; i<box_list.length; i++){
+
+      rcindex += 1
+   // for(i = 0; i<box_list.length; i++){
+    if (box_list.length > 0){
+      
+      i = getRandomInt(box_list.length)
+      console.log("RANDOM:"+i)
+      console.log("list: "+box_list[i])
       elem = box_list[i]
-      elem_x = (elem[0] + elem[1]) / 2
-      elem_y = (elem[2] + elem[3]) / 2
-      bot_move.click(0, 0);
 
-    }
-  });
-}
-var predict_req = http.request(predict_options, predict_callback);
-
-var req = http.request(options, function(res) {
-    res.setEncoding('utf8')
-    var responseString = "";
-
-    res.on("data", function (data) {
-        responseString += data;
-    });
-    res.on("end", function (){
-        console.log("TEST!");
-        console.log(responseString)
-    });
-
+      elem_x = Number(((elem[0] + elem[2]) / 2).toFixed(2));
+      elem_y = Number(((elem[1] + elem[3]) / 2).toFixed(2));
+      console.log(elem_x, elem_y)
+      robot_control(elem_x, elem_y);
+      curtime = (parseFloat(Date.now()/1000) - start_time)
+      console.log("[ROBOT COUNT INDEX] index + 1")
+      csv_data.push({'x-axis': elem_x, 'y-axis': elem_y, 'curtime': curtime})
+     }
+    //waitSync(2);
+    console.log("END");  
 });
+ }
+//var predict_req = http.request(predict_options, predict_callback);
+
+
+//var req = http.request(options, function(res) {
+ //   res.setEncoding('utf8')
+ //   var responseString = "";
+
+//    res.on("data", function (data) {
+//        responseString += data;
+//    });
+//    res.on("end", function (){
+//        console.log("TEST!");
+//        console.log(responseString)
+//    });
+
+//});
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 
 function collectRequestData(request, callback) {
@@ -94,47 +117,117 @@ function collectRequestData(request, callback) {
    }
 
 
+function start_experiment(start_time, count, filename){
+  while(rcindex < count){
+
+    console.log("[ROBOT COUNT INDEX] "+ rcindex)
+    http.request(predict_options, predict_callback).end();
+    waitSync(10)
+  }
+  splitStr = filename.split('_')
+  device = splitStr[0]
+  appname = splitStr[1]
+  //write_csv(device, appname);
+  waitSync(2);
+  console.log("DONE");
+}
+
 function onRequest(request, response){
     var pathName = url.parse(request.url).pathname;
     console.log('method: ')
     console.log(request.method)
     if(request.method == 'POST'){
         data = 'recording'
-        req.write(data)
-
+        //req.write(data)
 
         collectRequestData(request, result => {
                 var post_data_key = Object.keys(result);
-                for(var item of post_data_key){
-                    console.log('item'+item.toString())
-                    if(item.toString() == 'starttime'){
-                      start_time = result['starttime'];
-                      send_msg(response, "OK");
-                      http.request(predict_options, predict_callback).end()
-                      }
-                    else if(item.toString() == 'count'){
-                        count = result['count']
-                        control_robot(result)}
-
-                    else if (item.toString() == 'xy-axis'){
-                        json_xy = result['xy-axis']
-
-                        xy = JSON.parse(json_xy)
-                        appname = xy.appname
-                        x_axis = parseInt(xy.x, 10)
-                        y_axis = parseInt(xy.y, 10)
-
-                        console.log(appname, x_axis, y_axis)
-                        //control_robot(result)
-                        bot_move.click(x_axis, y_axis)
-                        console.log("BOT MOVE BYE");
+                if(post_data_key.includes("source")){
+                    source = result['source']
+                    if(source == 'recording'){
+                      if(result['status'] == 'start'){
+                        start_time = result['starttime'];
+                        appname = result['appname']
+                        count = 5//result['count']
                         send_msg(response, "OK");
+                        is_going = true
+                        console.log("START EX");
+                        rcindex = 0;
+                        start_experiment(start_time,count,appname);
+                        }
+                      else{
+                        is_going = false
+                        send_json_msg(response, csv_data);
+                        csv_data = []
+                        console.log('sendOK')
+                      }
                     }
-                }
+                   else if(source == 'recording-passive'){
+                      if(result['status'] == 'start'){
+                        start_time = result['starttime'];
+                        console.log("START TIME: "+ start_time);
+                        appname = result['appname']
+                        send_msg(response, "OK");
+                        is_going = true
+                        console.log("START EX");
+                        rcindex = 0;
+                      }
+                      else if(result['status'] == 'appexecute'){
+                        console.log("appexecute");
+                        appexecute_time = result['execute']
+                        csv_data.push({'x-axis': 0, 'y-axis': 0, 'curtime': appexecute_time-start_time, 'load-time':0})
+                        console.log('appexecute_time:'+ appexecute_time);
+                        appname = result['appname']
+                        send_msg(response, "OK")
+                      }
+                      else if(result['status'] == 'on-going'){
+                          x = result['xy-axisx']
+                          y = result['xy-axisy']
+                          z = result['xy-axisz']
+                          //xy = JSON.parse(json_xy)
+                          appname = result['appname']
+                          console.log("[DEBUG]: ",result)
+                          x_axis = parseInt(x, 10)
+                          y_axis = parseInt(y, 10)
+                          z_axis = parseInt(z, 10)
+                          console.log(appname, x_axis, y_axis, z_axis)
+                          control_robot(x_axis, y_axis)
+                          //bot_move.click(x_axis, y_axis, z_axis)
+                          console.log("BOT MOVE BYE");
+                          send_msg(response, "OK");
+                      }
+                     else{
+                        send_json_msg(response, csv_data);
+                        csv_data = []
+                        console.log('sendOK')
+                        bot_move_time = 0
+                     } 
+
+                   }
+                   else if(source == 'click-test'){
+                          json_xy = result['xy-axis']
+                          xy = JSON.parse(json_xy)
+                          appname = xy.appname
+                          x_axis = parseInt(xy.x, 10)
+                          y_axis = parseInt(xy.y, 10)
+                          z_axis = parseInt(xy.z, 10)
+                          console.log(appname, x_axis, y_axis, z_axis)
+                          control_robot(result)
+                          //bot_move.click(x_axis, y_axis, z_axis)
+                          console.log("BOT MOVE BYE");
+                          send_msg(response, "OK");
+                      }
+                    else{
+                      if(item.toString() == 'count'){
+                          count = result['count']
+                          control_robot(result)}
+                    }
+               }
             });
         }
     
-    else{
+    else if(request.method == 'GET') {
+        console.log("SHOWPAGE"); 
         showPage(response, pathName)
     }
 
@@ -143,43 +236,43 @@ function onRequest(request, response){
 
 //TODO bot control app server를 따로 만들어야함!
 
-function control_robot(result){
-    
-    start_time = parseFloat(Date.now())
-    for(i = 0; i <  result['count']; i++){
+function control_robot(x, y){
+    load_start_time = parseFloat(Date.now())
+    setTimeout(function(){bot_move.click(x, y)},1000)
+    curtime = parseFloat(Date.now())/1000
+    load_end_time = parseFloat(Date.now()) 
+    load_time = (load_end_time - load_start_time)
+    console.log("load_time: "+load_time)
+    console.log("curtime:"+curtime)
+    csv_data.push({'x-axis': x, 'y-axis': y, 'curtime': curtime-start_time, 'load-time':load_time})
+    bot_move_time += 1000
+}
 
-        load_start_time = parseFloat(Date.now())
+function robot_control(x, y){
+  bot_move.click(x, y);
 
-        bot_move.move()
-        waitSync(1)
 
-        curtime = (parseFloat(Date.now()) - start_time)/1000
+}
 
-        load_end_time = parseFloat(Date.now()) 
-        load_time = (load_end_time - load_start_time)/1000
-
-        console.log("load_time: "+load_time)
-        console.log("curtime:"+curtime)
-        csv_data.push({'x-axis': x, 'y-axis': y, 'curtime': curtime, 'load-time':load_time})
-
-        waitSync(2)}
-
-        write_csv()
+String.prototype.format = function(){
+  a = this;
+  for (k in arguments){
+    a = a.replace("{"+k+"}", arguments[k])
+    }
+  return a
 }
 
 
-
-
-function write_csv(){
+function write_csv2(device, appname){
     
-    var createCsvWriter = csv_writer.createObjectCsvWriter; 
-    var csvWriter = createCsvWriter({                                       
-    path: '/home/kimsoohyun/00-Research/02-Graph/05-data/cut_point/cut_info.csv', 
+    createCsvWriter = csv_writer.createObjectCsvWriter; 
+
+    csvWriter = createCsvWriter({                                       
+    path: '/home/kimsoohyun/00-Research/02-Graph/05-data/cut_point/{0}/{1}.csv'.format(device, appname), 
     header: [                                                                   
         {id: 'x-axis', title: 'x-axis'},                                        
         {id: 'y-axis', title: 'y-axis'},                                         
         {id: 'curtime',    title: 'cur_time'},                                  
-        {id: 'load-time', title: 'load-time'}                                   
     ]                                                                           
     });
     csvWriter
@@ -219,8 +312,17 @@ function showPage(response, pathName){
 
 function send_msg(response,data){
     response.writeHead(200, {'Content-Type': 'text/html'});         
-    console.log("TEST!")
     console.log(data);                                              
     response.write(data);                                           
+    response.end(); 
+}
+
+
+function send_json_msg(response, data){
+    response.writeHead(200, {'Content-Type': 'text/html'});         
+    console.log("TEST!")
+    str_data = JSON.stringify(data)
+    console.log(str_data);                                              
+    response.write(str_data);                                           
     response.end(); 
 }
